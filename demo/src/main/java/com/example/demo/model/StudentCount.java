442a5bf74f9fb556ed5eb5d4c2d06168440bb860
rana722300 <rana722300@gmail.com>
2023-12-28 23:47:07 -0800

    Added query

--- demo/src/main/java/com/example/demo/model/StudentCount.java
+++ demo/src/main/java/com/example/demo/model/StudentCount.java
@@ -2,9 +2,10 @@ package com.example.demo.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@Component
 @Data
 @Builder
 public class StudentCount {
--- demo/src/main/java/com/example/demo/repository/StudentRepository.java
+++ demo/src/main/java/com/example/demo/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.domain.ClassLevel;
 import com.example.demo.domain.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public interface StudentRepository extends MongoRepository<Student, UUID> {
 
     List<Student> getAllByHuman_NameAndHuman_FatherName(String name, String fatherName);
 
+    @Query(fields = "{'fees':1}")
     List<Student> getAllByHuman_Address_City(String city);
 
     List<Student> getAllByHuman_Address_district(String district);
